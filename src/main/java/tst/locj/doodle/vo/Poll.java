2ba9b142391445d0fde14199541809f80a702dc4
laerteocj <link.alekine@gmail.com>
2020-07-07 02:33:42 +0200

    Adjusting the Poll and Test class

--- src/main/java/tst/locj/doodle/vo/Poll.java
+++ src/main/java/tst/locj/doodle/vo/Poll.java
@@ -1,13 +1,17 @@
 package tst.locj.doodle.vo;
 
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 import java.time.Instant;
 import java.util.List;
 import java.util.Locale;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * Data object that defines a Poll entry on the system, contains all the information need to handle
@@ -15,21 +19,22 @@ import lombok.Data;
  */
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonTypeInfo(use = Id.NAME, property = "type")
+@JsonTypeInfo(use = NAME, property = "type", include = As.EXISTING_PROPERTY, visible = true)
 @JsonSubTypes(
     value = {
       @JsonSubTypes.Type(value = TextPoll.class, name = "TEXT"),
       @JsonSubTypes.Type(value = DatePoll.class, name = "DATE")
     })
+@Document
 public class Poll {
 
-  private String id;
+  @Id private String id;
   private String adminKey;
   private Instant lastestChange;
   private Instant initiated;
   private int participantsCount;
   private int inviteesCount;
-  private PollType type;
+  private String type;
   private int rowConstraint;
   private PreferencesType preferencesType;
   private PollState state;
--- src/test/java/tst/locj/doodle/DemoApplicationTests.java
+++ src/test/java/tst/locj/doodle/DemoApplicationTests.java
@@ -1,34 +1,14 @@
 package tst.locj.doodle;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import java.util.List;
-import java.util.Map;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import tst.locj.doodle.vo.Poll;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@DataMongoTest
-@ExtendWith(SpringExtension.class)
-class DemoApplicationTests {
-
-  private ObjectMapper objectMapper = new ObjectMapper();
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DemoApplicationTests {
 
   @Test
-  void contextLoads() throws Exception {
-    objectMapper.registerModule(new JavaTimeModule());
-    ClassPathResource resource = new ClassPathResource("assets/polls.json");
-    List<Poll> polls =
-        objectMapper.readValue(resource.getInputStream(), new TypeReference<List<Poll>>() {});
-    System.out.println(polls);
-
-    List<Map<String, Object>> maps =
-        objectMapper.readValue(
-            resource.getInputStream(), new TypeReference<List<Map<String, Object>>>() {});
-    System.out.println(maps);
-  }
+  public void contextLoads() throws Exception {}
 }
